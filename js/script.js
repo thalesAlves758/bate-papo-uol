78bb83d55d9036f45eef3838536f2ee48893ba5a
unknown <thalesalves758@gmail.com>
2022-04-13 23:23:41 -0300

    Carrega as mensagens do servidor e exibe-as conforme o layout

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+let messages = [];
+
 const showSideMenu = () => {
     document.querySelector('.side-menu-container').classList.remove('hidden');
 };
@@ -15,3 +17,50 @@ const selectItem = itemEl => {
     if(selectedItemEl) selectedItemEl.classList.remove('selected');
     itemEl.classList.add('selected');
 };
+
+const getMessageTypeClass = messageType => {
+    switch(messageType) {
+        case 'status':
+            return 'status';
+
+        case 'message':
+            return 'normal';
+
+        case 'private_message':
+            return 'reserved';
+    }
+};
+
+const getReservedString = message => {
+    return `${message.type === 'private_message' ? 'reservadamente ' : ''}`;
+};
+
+const getReceiverMessage = message => {
+    return `${message.type !== 'status' ? `${getReservedString(message)}para <span class="username">${message.to}</span>:` : ''}`;
+};
+
+const renderMessages = () => {
+    const messageElements = messages.map(message => {
+        return `<div class="message ${getMessageTypeClass(message.type)}-message">
+            <p>
+                <span class="hour">(${message.time})</span>
+                <span class="username">${message.from}</span>
+                ${getReceiverMessage(message)}
+                ${message.text}
+            </p>
+        </div>`;
+    });
+
+    document.querySelector('.messages-container').innerHTML = messageElements.length > 0 ? messageElements.join('') : 'Nenhuma mensagem!';
+};
+
+const getMessages = () => {
+    axios
+        .get("https://mock-api.driven.com.br/api/v6/uol/messages")
+        .then(response => {
+            messages = response.data;
+            renderMessages();
+        });
+};
+
+getMessages();
